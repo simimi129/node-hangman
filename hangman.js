72db887869f39d8eb50869b3459cd0b8ecb2c04a
simimi129 <simimi129@gmail.com>
2025-10-06 21:17:45 +0200

    tousch up

--- hangman.js
+++ hangman.js
@@ -1,5 +1,4 @@
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,25 +6,22 @@ const rl = readline.createInterface({
 
 const word = "testing";
 const wordArray = word.split("");
-
 const guessedLetters = new Array(wordArray.length).fill("_");
 let guessesLeft = 5;
-
 const triedLetters = new Set();
 
 function question(query) {
   return new Promise((resolve) => rl.question(query, resolve));
 }
 
-console.log(`word: ${guessedLetters.join(" ")}, guesses left: ${guessesLeft}`);
-
+console.log(`Word: ${guessedLetters.join(" ")}, guesses left: ${guessesLeft}`);
 (async () => {
   while (true) {
     const guess = await question("Take a guess: ");
     const normalizedGuess = guess.toLowerCase();
 
-    if (normalizedGuess.length > 1) {
-      console.log("Please write one letter only!");
+    if (!/^[a-z]$/i.test(normalizedGuess)) {
+      console.log("Please enter a single letter (A-Z)!");
       continue;
     }
 
@@ -37,7 +33,7 @@ console.log(`word: ${guessedLetters.join(" ")}, guesses left: ${guessesLeft}`);
 
     if (wordArray.includes(normalizedGuess)) {
       console.log("Good guess!");
-      wordArray.map((l, i) =>
+      wordArray.forEach((l, i) =>
         l === normalizedGuess ? (guessedLetters[i] = normalizedGuess) : null
       );
       if (!guessedLetters.includes("_")) {
@@ -53,8 +49,9 @@ console.log(`word: ${guessedLetters.join(" ")}, guesses left: ${guessesLeft}`);
       }
     }
     console.log(
-      `\nword: ${guessedLetters.join(" ")}, guesses left: ${guessesLeft}`
+      `\nWord: ${guessedLetters.join(" ")} | guesses left: ${guessesLeft}`
     );
+    console.log(`Tried letters: ${Array.from(triedLetters).join(", ")}`);
   }
   rl.close();
 })();
